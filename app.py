3ae47db268a0ae0ea0da6e1a2f448a6d702db081
AndErem314 <andrey@Mac.lan>
2025-06-23 20:48:24 +0200

    implement errors and style.css

--- app.py
+++ app.py
@@ -18,6 +18,21 @@ db.init_app(app)
 data_manager = DataManager()
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('errors/404.html'), 404
+
+
+@app.errorhandler(403)
+def forbidden(e):
+    return render_template('errors/403.html'), 403
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('errors/500.html'), 500
+
+
 @app.route('/')
 def home():
     users = data_manager.get_users()
